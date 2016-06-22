834f252f3262e2c86d3e9f2e84aa4c66a53a793b
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-06-22 18:32:48 +0200

    Fix Falcor cache article indexing bug

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -11,7 +11,7 @@ export default class Navigation extends React.Component {
           <li><Link to="/page/0">Page 0</Link></li>
           <li><Link to="/page/1">Page 1</Link></li>
           <li><Link to="/page/2">Page 2</Link></li>
-          <li><Link to="/article/1">Article 1</Link></li>
+          <li><Link to="/article/0">Article 1</Link></li>
         </ul>
       </div>
     )
