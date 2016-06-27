b2ea8b147d7e614332de536b9779ccbbecb9f63c
Emil Goldsmith Olesen <emil_12345@hotmail.com>
2016-06-27 17:37:31 +0200

    fixed redirectLocation.serach to redirectLocation.search

--- src/server.js
+++ src/server.js
@@ -101,7 +101,7 @@ server.get('*', (req, res) => {
       if (error) {
         res.status(500).send(error.message);
       } else if (redirectLocation) {
-        res.redirect(302, redirectLocation.pathname + redirectLocation.serach);
+        res.redirect(302, redirectLocation.pathname + redirectLocation.search);
       } else if (renderProps) {
         renderApp(renderProps).then((html) => {
           res.status(200).send(html);
