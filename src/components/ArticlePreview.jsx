a6ffd9f329beb2036cef508d8f00d5bf568d6bab
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-07-16 13:41:04 +0800

    Clean up ArticlePreview

--- src/components/ArticlePreview.jsx
+++ src/components/ArticlePreview.jsx
@@ -9,18 +9,15 @@ export default class ArticlePreview extends React.Component {
     // Render all contributing authors
     function renderAuthors() {
       // Return nothing if no authors listed
-      // DEV: (article.authors || []).map((author) => {
       let data = [];
       if (article.authors){
         for (var author in article.authors){
           if (article.authors.hasOwnProperty(author)) {
             data.push(
               <div>
-                <span>
-                  <Link to={'/author/' + article.authors[author].slug}>
-                    {article.authors[author].name}
-                  </Link>
-                </span>
+                <Link to={'/author/' + article.authors[author].slug}>
+                  {article.authors[author].name}
+                </Link>
               </div>
             );
           }
@@ -37,12 +34,10 @@ export default class ArticlePreview extends React.Component {
         */}
 
         {/* Author(s) */}
-        By {renderAuthors()}
-        {console.log("Author 0: " + article.authors[0].name)}
+        {renderAuthors()}
 
         {/*
           Article title with link to article
-          Link format: thegazelle.org/issue/:issueId/:articleCategory/:articleSlug
         */}
         <Link to={'/issue/' + article.issueId + '/' + article.category + '/' + article.slug}>
           {article.title}
