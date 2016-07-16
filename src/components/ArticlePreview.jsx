935ffbb38c3e7ee92bb2b284cb1095e0a150fa31
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-07-16 13:33:32 +0800

    Render articles in category as ArticlePreviews

--- src/components/ArticlePreview.jsx
+++ src/components/ArticlePreview.jsx
@@ -4,18 +4,30 @@ import { Link } from 'react-router';
 export default class ArticlePreview extends React.Component {
 
   render () {
-    // Render all contributing authors
-    // function renderAuthors() {
-    //   // Return nothing if no authors listed
-    //   return (article.authors || []).map((author) => {
-    //       <div>
-    //         <Link to={'/author/' + author.slug}>
-    //           {author.name}
-    //         </Link>
-    //       </div>
-    //   });
-    // }
+    let article = this.props.article;
 
+    // Render all contributing authors
+    function renderAuthors() {
+      // Return nothing if no authors listed
+      // DEV: (article.authors || []).map((author) => {
+      let data = [];
+      if (article.authors){
+        for (var author in article.authors){
+          if (article.authors.hasOwnProperty(author)) {
+            data.push(
+              <div>
+                <span>
+                  <Link to={'/author/' + article.authors[author].slug}>
+                    {article.authors[author].name}
+                  </Link>
+                </span>
+              </div>
+            );
+          }
+        }
+      }
+      return data;
+    }
     return (
       <div>
         {/*
@@ -25,20 +37,19 @@ export default class ArticlePreview extends React.Component {
         */}
 
         {/* Author(s) */}
-        {/*renderAuthors()*/}
+        By {renderAuthors()}
+        {console.log("Author 0: " + article.authors[0].name)}
 
         {/*
           Article title with link to article
           Link format: thegazelle.org/issue/:issueId/:articleCategory/:articleSlug
         */}
-        {/*}<Link to={'/issue/' + article.issueId + '/' + article.category + '/' + article.slug}>
-          {this.props.title}
+        <Link to={'/issue/' + article.issueId + '/' + article.category + '/' + article.slug}>
+          {article.title}
         </Link>
-        */}
 
         {/* Article teaser */}
-        <h2>{this.props.title}</h2>
-        <p>{this.props.teaser}</p>
+        <p>{article.teaser}</p>
       </div>
     );
   }
@@ -46,6 +57,12 @@ export default class ArticlePreview extends React.Component {
 
 // Validate shape of article JSON object
 ArticlePreview.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  teaser: React.PropTypes.string.isRequired,
+  article: React.PropTypes.shape({
+    title: React.PropTypes.string.isRequired,
+    teaser: React.PropTypes.string.isRequired,
+    issueId: React.PropTypes.string.isRequired,
+    category: React.PropTypes.string.isRequired,
+    slug: React.PropTypes.string.isRequired,
+    authors: React.PropTypes.object,
+  }),
 }
--- src/components/CategoryController.jsx
+++ src/components/CategoryController.jsx
@@ -9,24 +9,40 @@ export default class CategoryController extends FalcorController {
     // Multilevel request requires Falcor Path for each level of data requested
     // Grab first 10 articles for category requested
     return [
-      ["categories", params.category, {to: 10}, ["title", "teaser"]],
+      ["categories", params.category, {to: 10}, ["title", "teaser", "issueId", "category", "slug", "uuid"]],
+      ["categories", params.category, {to: 10}, "authors", {to: 10}, ["name", "slug"]],
     ];
   }
 
   render() {
-    console.log("RENDERING CATEGORY CONTROLLER");
     if (this.state.ready) {
+      console.log("RENDERING CATEGORY CONTROLLER");
       let category = this.props.params.category;
       const categoryData = this.state.data.categories[category];
+
+      // Render all articles in category as ArticlePreviews
+      let renderArticles = () => {
+        let data = [];
+        for (let article in categoryData) {
+          if (categoryData.hasOwnProperty(article)) {
+            data.push(
+              <ArticlePreview
+                key={categoryData[article].uuid}
+                article={categoryData[article]}
+              />
+            )
+            console.log("Article: " + categoryData[article].title);
+          }
+        }
+        return data;
+      }
+
       console.log("Data: " + JSON.stringify(categoryData));
       return (
         <div>
           <div>Controller for category: {category}</div>
           <div>Ready?: {this.state.ready ? 'true' : 'false'}</div>
-          <ArticlePreview
-            title={categoryData[0].title}
-            teaser={categoryData[0].teaser}
-          />
+          {renderArticles()}
         </div>
       );
     } else {
