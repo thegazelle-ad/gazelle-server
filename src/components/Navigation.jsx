9176d2fd174deeb4e1bfb7f820d92d321f135ee3
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-07-16 11:11:08 +0800

    Add Navigation prop type validation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 export default class Navigation extends React.Component {
 
   render() {
+
     // Render all categories in nav bar
     let renderCategories = () => {
       let categories = ["opinion", "features", "news"];
@@ -21,15 +22,15 @@ export default class Navigation extends React.Component {
     return (
       <div>
         <h1>{this.props.appName}</h1>
-        {/* Add list item by categories that exist in issue x */}
         <ul>
           <li><Link to="/">Home</Link></li>
           {renderCategories()}
-
-          {/*<li><Link to="/issue/55/opinion/palestine-hamilton">Sample Article</Link></li>
-          <li><Link to="/author/ahmed-meshref">Sample Author</Link></li>*/}
         </ul>
       </div>
     )
   }
+}
+
+Navigation.propTypes = {
+  appName: React.PropTypes.string,
 }
