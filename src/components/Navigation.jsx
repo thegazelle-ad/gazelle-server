03777612e628e1256ebdcd979b0732610f5511f2
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-07-19 18:51:06 +0800

    Changed category loop to Lodash map

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 import { Link } from 'react-router';
 
 export default class Navigation extends React.Component {
@@ -6,25 +7,22 @@ export default class Navigation extends React.Component {
   render() {
 
     // Render all categories in nav bar
-    let renderCategories = () => {
-      let categories = ["opinion", "features", "news"];
-      let data = [];
-
-      categories.forEach(function(category) {
-        data.push(
-          <li key={category}><Link to={"/category/"+category}>{category}</Link></li>
+    let categories = ["opinion", "features", "news"];
+    let renderCategories =
+      _.map((categories || []), function(category) {
+        return(
+          <li key={category}>
+            <Link to={"/category/"+category}>{category}</Link>
+          </li>
         )
       });
-
-      return data
-    }
 
     return (
       <div>
         <h1>{this.props.appName}</h1>
         <ul>
           <li><Link to="/">Home</Link></li>
-          {renderCategories()}
+          {renderCategories}
         </ul>
       </div>
     )
