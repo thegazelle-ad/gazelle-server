457a4c9ee98e37fdd0bac537caf7cd55e0cc482f
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-07-24 18:30:15 +0800

    Added basic media queries

--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,10 @@ module.exports = [{
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract(['css','sass']),
       },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
+      },
     ],
   },
   devtool: 'source-map',
