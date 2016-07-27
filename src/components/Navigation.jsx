814fe3390c23945094d7a44e1aa785127ac52eb3
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-07-27 15:36:39 +0800

    Add last navigation components with hardcoded content

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,14 +5,13 @@ import { Link } from 'react-router';
 export default class Navigation extends React.Component {
 
   render() {
-
     // Render all categories in nav bar
     let categories = ["off-campus", "on-campus", "commentary", "creative", "in-focus"];
     let renderCategories =
       _.map((categories || []), function(category) {
         return(
-          <li key={category} className="navigation__item">
-            <Link to={"/category/"+category} activeClassName="navigation__item--active">
+          <li key={category} className="navigation__categories__item">
+            <Link to={"/category/"+category} activeClassName="navigation__categories__item--active">
               {category}
             </Link>
           </li>
@@ -21,12 +20,29 @@ export default class Navigation extends React.Component {
 
     return (
       <div>
-        <div className="header__title">
-          <Link to="/">{this.props.appName}</Link>
+        <div className="header">
+          <div className="header__search">
+            <input
+              className="header__search__main"
+              activeClassName="header__search__main--active"
+              type="text"
+            />
+            <div className="header__search__tail" />
+          </div>
+          <div className="header__title">
+            <Link to="/" className="header__title--content">{this.props.appName}</Link>
+          </div>
+          {/* TODO: change link to archives list */}
+          <Link to="/" className="header__issueId">{"Issue 55"}</Link>
         </div>
-        <ul className="navigation">
-          {renderCategories}
-        </ul>
+        <div className="navigation">
+          <p className="navigation__publication-date">Aug. 15, 2016</p>
+          <ul className="navigation__categories">
+            {renderCategories}
+          </ul>
+          <p className="navigation__weather">35&deg;</p>
+        </div>
+
       </div>
     )
   }
