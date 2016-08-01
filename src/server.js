4ca140cf672d742bf3ce47e4805f78b62065ca78
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-08-01 09:17:40 +0800

    Fixed favicon paths

--- src/server.js
+++ src/server.js
@@ -29,11 +29,11 @@ const buildHtmlString = (body, cache) => {
         <head>
           <title>The Gazelle</title>
           <link rel="stylesheet" type="text/css" href="/static/build/main.css">
-          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png">
-          <link rel="icon" type="image/png" href="/static/favicons/favicon-32x32.png" sizes="32x32">
-          <link rel="icon" type="image/png" href="/static/favicons/favicon-16x16.png" sizes="16x16">
-          <link rel="manifest" href="/static/favicons/manifest.json">
-          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5">
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+          <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+          <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+          <link rel="manifest" href="/favicons/manifest.json">
+          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
           <meta name="theme-color" content="#ffffff">
           <meta name="viewport" content="width=device-width, initial-scale=1">
         </head>
