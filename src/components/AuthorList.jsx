3d615fcf8efafd718d8b6aa18b7a88218f90a104
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-08-02 09:31:59 +0800

    Fixed featured article image width bug

--- src/components/AuthorList.jsx
+++ src/components/AuthorList.jsx
@@ -6,7 +6,7 @@ export default function AuthorList(props) {
   // Render all authors
   let renderAuthors =
     // Render nothing if props.authors is empty
-    _.map((props.authors || []), function(author) {
+    _.map((props.authors || []), (author) => {
       return(
         <li key={author.slug} className="author-list__author">
           <Link to={'/author/' + author.slug}>
