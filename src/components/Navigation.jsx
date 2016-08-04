6a9612c4e6e94c29dc2f06bf0204f4919872ea94
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2016-08-04 16:06:25 +0200

    fixed weird syntax issues to make it compile

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
+import BaseComponent from 'lib/BaseComponent';
 import _ from 'lodash';
 import { Link } from 'react-router';
 
-export default function Navigation(props) {
-  // Render all categories in nav bar
-  let categories = ["off-campus", "on-campus", "commentary", "creative", "in-focus"];
-  let renderCategories =
-    _.map((categories || []), function(category) {
-      return(
-        <li key={category} className="navigation__categories__item">
-          <Link to={"/category/"+category} activeClassName="navigation__categories__item--active">
-            {category}
-          </Link>
-        </li>
-      )
-    });
-
+export default class Navigation extends BaseComponent {
   render() {
     // Render all categories in nav bar
     let categories = ["off-campus", "on-campus", "commentary", "creative", "in-focus"];
@@ -32,31 +20,34 @@ export default function Navigation(props) {
 
     return (
       <div>
-        <div className="header">
-          <div className="header__search">
-            <input
-              className="header__search__main"
-              type="text"
-              placeholder="Search The Gazelle"
-            />
-            <div className="header__search__tail" />
-            <div className="header__search__text">SEARCH</div>
+        <div>
+          <div className="header">
+            <div className="header__search">
+              <input
+                className="header__search__main"
+                type="text"
+                placeholder="Search The Gazelle"
+              />
+              <div className="header__search__tail" />
+              <div className="header__search__text">SEARCH</div>
+            </div>
+            <div className="header__title">
+              <Link to="/" className="header__title--content">{this.props.appName}</Link>
+            </div>
+            {/* TODO: change link to archives list */}
+            <Link to="/" className="header__issueId">{"Issue 55"}</Link>
           </div>
-          <div className="header__title">
-            <Link to="/" className="header__title--content">{this.props.appName}</Link>
-          </div>
-          {/* TODO: change link to archives list */}
-          <Link to="/" className="header__issueId">{"Issue 55"}</Link>
+        </div>
+        <div className="navigation">
+          <p className="navigation__publication-date">Aug. 15, 2016</p>
+          <ul className="navigation__categories">
+            {renderCategories}
+          </ul>
+          <p className="navigation__weather">35&deg;</p>
         </div>
       </div>
-      <div className="navigation">
-        <p className="navigation__publication-date">Aug. 15, 2016</p>
-        <ul className="navigation__categories">
-          {renderCategories}
-        </ul>
-        <p className="navigation__weather">35&deg;</p>
-      </div>
-  );
+		);
+	}
 }
 
 Navigation.propTypes = {
