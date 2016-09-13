3e9f3fcef3083951898976b58879dcc4fa514a14
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2016-09-13 12:02:01 +0400

    Added databaseName variable to backupDatabase.example.sh

--- scripts/backupDatabase.example.sh
+++ scripts/backupDatabase.example.sh
@@ -3,10 +3,12 @@
 # To get your own nodePath run `which node` in your terminal and copy the output
 # to the nodePath variable
 nodePath=""
+# Enter the name of the mariaDB database to dump from here.
+databaseName=""
 directive=$(dirname $0)
 
 # printf "Dumping database temporarily to temp.dump\n"
-mysqldump -u root -ppassword live_gazelle > "$directive/helperFiles/temp.dump"
+mysqldump -u root -ppassword "$databaseName" > "$directive/helperFiles/temp.dump"
 # printf "\ndump successful\n"
 # printf "\ninitiating database backup\n"
 eval "$nodePath $directive/helperFiles/uploadDatabaseDump.js $directive/helperFiles/temp.dump"
