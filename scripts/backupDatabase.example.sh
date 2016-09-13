546f9f5ccbbd653498c6b98196873811c7529eb2
zanemountcastle <zane.mountcastle@gmail.com>
2016-09-13 08:23:51 +0000

    added more database variables to backupDatabase script

--- scripts/backupDatabase.example.sh
+++ scripts/backupDatabase.example.sh
@@ -5,10 +5,14 @@
 nodePath=""
 # Enter the name of the mariaDB database to dump from here.
 databaseName=""
+# Enter the name of the mysql user
+userName=""
+# Enter the password of the mysql user
+password=""
 directive=$(dirname $0)
 
 # printf "Dumping database temporarily to temp.dump\n"
-mysqldump -u root -ppassword "$databaseName" > "$directive/helperFiles/temp.dump"
+eval "mysqldump -u $userName -p$password $databaseName > $directive/helperFiles/temp.dump"
 # printf "\ndump successful\n"
 # printf "\ninitiating database backup\n"
 eval "$nodePath $directive/helperFiles/uploadDatabaseDump.js $directive/helperFiles/temp.dump"
