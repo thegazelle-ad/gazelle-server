96bb0e6eee26eb76a8215d95f21d32e53907efa7
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2016-09-20 15:03:31 +0400

    Upped maxSize of falcor cache

--- src/server.js
+++ src/server.js
@@ -80,8 +80,8 @@ const buildHtmlString = (body, cache) => {
 // You can also hardcode / stub parts of the model here
 const serverModel = new falcor.Model({
   source: new FalcorRouter(),
-  // maxSize is 300 MB in production and 50 MB when in development or beta mode
-  maxSize: process.env.NODE_ENV === "production" ? 300*1000*1000 : 50*1000*1000,
+  // maxSize is 500 MB in production and 100 MB when in development or beta mode
+  maxSize: process.env.NODE_ENV === "production" ? 500*1000*1000 : 100*1000*1000,
   collectRatio: 0.75,
 }).batch();
 
