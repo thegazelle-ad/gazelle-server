3a719c75d7142e059d2c0b470d75c88436fabc12
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2016-10-09 15:07:23 +0400

    fixed linting error

--- src/components/Archives.jsx
+++ src/components/Archives.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import BaseComponent from 'lib/BaseComponent';
 import { Link } from 'react-router';
-import _ from 'lodash';
 import moment from 'moment';
 
 export default class Archives extends BaseComponent {
