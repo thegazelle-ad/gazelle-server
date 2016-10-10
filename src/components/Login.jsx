daf07201c5a2ae80b621ede15e58065e64d5e249
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-10-10 19:31:29 +0400

    Fixed login page width

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -56,43 +56,37 @@ export default class Login extends BaseComponent {
 
     return(
       <div className="login-container">
-        <div className="align">
-          <div className="site__container">
-            <div className="grid__container">
-              <h2>Gazelle Beta Login Page</h2>
-              <p>
-                Welcome to the Gazelle Beta, if you were looking for the
-                main Gazelle webpage please follow <a href={"https://www.thegazelle.org" + url}>
-                this link</a>. This page is only meant for The Gazelle Team to test
-                out new features before we release them on the main page. <br /><br />
-                If you on the other hand are part of The Gazelle and want to use the
-                beta page, login below with the password you have been given
-              </p>
-              <div className="login">
-                <form className="form form--login" onSubmit={this.handleSubmit}>
-                  <div className="form__field">
-                    <label className="fontawesome-lock" htmlFor="login__password"><span className="hidden">Password</span></label>
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                      id="login__password"
-                      className="form__input"
-                      autoFocus
-                      required
-                    />
-                  </div>
-
-                  <div className="form__field">
-                    <input
-                      type="submit"
-                      value="Enter Editor Tools"
-                    />
-                  </div>
-                </form>
-              </div>
+        <h2>Gazelle Beta Login Page</h2>
+        <p>
+          Welcome to the Gazelle Beta, if you were looking for the
+          main Gazelle webpage please follow <a href={"https://www.thegazelle.org" + url}>
+          this link</a>. This page is only meant for The Gazelle Team to test
+          out new features before we release them on the main page. <br /><br />
+          If you on the other hand are part of The Gazelle and want to use the
+          beta page, login below with the password you have been given
+        </p>
+        <div className="login">
+          <form className="form form--login" onSubmit={this.handleSubmit}>
+            <div className="form__field">
+              <label className="fontawesome-lock" htmlFor="login__password"><span className="hidden">Password</span></label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                id="login__password"
+                className="form__input"
+                autoFocus
+                required
+              />
             </div>
-          </div>
+
+            <div className="form__field">
+              <input
+                type="submit"
+                value="Enter Editor Tools"
+              />
+            </div>
+          </form>
         </div>
       </div>
     );
