6b484409a016fc7aae1f5a5cb47bd3d332e9c670
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2016-11-02 10:10:01 +0400

    Handle removing old categories and adding new ones to issue when category of article in issue is changed

--- src/lib/db.js
+++ src/lib/db.js
@@ -752,43 +752,100 @@ export default class db {
       let updatesCalled = 0;
       let updatesReturned = 0;
       issues.forEach((issueId) => {
-        database.select('issues_posts_order.id as id', 'category_id', 'posts_order')
-        .from('issues_posts_order')
-        .innerJoin('posts_meta', 'posts_meta.id', '=', 'issues_posts_order.post_id')
-        .where('type', '=', 0)
+        // Get the current categories so we know if we have to add new ones
+        // or delete old ones
+        database.select('category_id', 'categories_order')
+        .from('issues_categories_order')
         .where('issue_id', '=', issueId)
-        .orderBy('category_id', 'ASC')
-        .orderBy('issues_posts_order.posts_order', 'ASC')
-        .then((postRows) => {
-          let lastCategory = null;
-          let order = 0;
-          const toUpdate = [];
-          postRows.forEach((row) => {
-            if (lastCategory !== row.category_id) {
-              lastCategory = row.category_id;
-              order = 0;
-            }
-            if (order !== row.posts_order) {
-              toUpdate.push({
-                id: row.id,
-                update: {
-                  posts_order: order,
-                },
+        .orderBy('categories_order', 'ASC')
+        .then((categoryRows) => {
+          database.select('issues_posts_order.id as id', 'category_id', 'posts_order')
+          .from('issues_posts_order')
+          .innerJoin('posts_meta', 'posts_meta.id', '=', 'issues_posts_order.post_id')
+          .where('type', '=', 0)
+          .where('issue_id', '=', issueId)
+          .orderBy('category_id', 'ASC')
+          .orderBy('issues_posts_order.posts_order', 'ASC')
+          .then((postRows) => {
+            let lastCategory = null;
+            let order = 0;
+            const toUpdate = [];
+            const newCategories = [];
+            postRows.forEach((row) => {
+              if (lastCategory !== row.category_id) {
+                lastCategory = row.category_id;
+                order = 0;
+                newCategories.push(row.category_id);
+              }
+              if (order !== row.posts_order) {
+                toUpdate.push({
+                  id: row.id,
+                  update: {
+                    posts_order: order,
+                  },
+                });
+              }
+              order++;
+            });
+            updatesCalled += toUpdate.length;
+            toUpdate.forEach((obj) => {
+              database('issues_posts_order')
+              .where('id', '=', obj.id)
+              .update(obj.update)
+              .then(() => {
+                updatesReturned++;
+                if (updatesReturned >= updatesCalled) {
+                  resolve(true);
+                }
               });
-            }
-            order++;
-          });
-          updatesCalled += toUpdate.length;
-          toUpdate.forEach((obj) => {
-            database('issues_posts_order')
-            .where('id', '=', obj.id)
-            .update(obj.update)
-            .then(() => {
-              updatesReturned++;
-              if (updatesReturned >= updatesCalled) {
-                resolve(true);
+            });
+            // Check if categories are still consistent
+            const newCategoriesWithOrder = [];
+            let consistent = true;
+            categoryRows.forEach((category) => {
+              if (newCategories.find((cat) => {return cat === category.category_id}) !== undefined) {
+                newCategoriesWithOrder.push(category);
+              }
+              else {
+                consistent = false;
               }
             });
+            newCategories.forEach((categoryId) => {
+              if (newCategoriesWithOrder.find((cat) => {return cat.category_id === categoryId}) === undefined) {
+                consistent = false;
+                const foundHole = newCategoriesWithOrder.some((cat, index) => {
+                  if (index !== cat.categories_order) {
+                    newCategoriesWithOrder.splice(index, 0, {category_id: categoryId, categories_order: index});
+                    return true;
+                  }
+                  return false;
+                });
+                if (!foundHole) {
+                  newCategoriesWithOrder.push({category_id: categoryId, categories_order: newCategoriesWithOrder.length});
+                }
+              }
+            });
+            if (!consistent) {
+              newCategoriesWithOrder.forEach((cat, index) => {
+                cat.categories_order = index;
+                cat.issue_id = issueId;
+              });
+              updatesCalled++;
+              // Delete the current categories order and insert the new one
+              database('issues_categories_order').where('issue_id', '=', issueId).del()
+              .then(() => {
+                database('issues_categories_order').insert(newCategoriesWithOrder)
+                .then(() => {
+                  updatesReturned++;
+                  if (updatesReturned >= updatesCalled) {
+                    resolve(true);
+                  }
+                });
+              });
+            }
+            else {
+              resolve(true);
+            }
           });
         });
       });
