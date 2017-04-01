b5c49a6760cff2f04a6ffa3cb825b4b59d55af0c
Zane Mountcastle <zane.mountcastle@gmail.com>
2017-04-01 19:40:45 -0400

    More author page improvements

--- src/components/editor/EditorAuthorController.jsx
+++ src/components/editor/EditorAuthorController.jsx
@@ -5,6 +5,9 @@ import _ from 'lodash';
 
 // material-ui
 import CircularProgress from 'material-ui/CircularProgress';
+import Divider from 'material-ui/Divider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const MAX_BIOGRAPHY_LENGTH = 400;
 
@@ -16,10 +19,35 @@ export default class EditorAuthorController extends FalcorController {
     this.safeSetState({
       changed: false,
       saving: false,
+      name: '',
+      slug: '',
+      job_title: '',
+      image: '',
       biography: '',
     })
 
+    this.debouncedHandleFormStateChanges = debounce( () => {
+      // We don't want the debounced event to happen if we're saving
+      if (this.state.saving) return;
+
+      const falcorData = this.state.data.authorsBySlug[this.props.params.slug];
+      const changedFlag =
+        this.isChanged(this.state.name, falcorData.name) ||
+        this.isChanged(this.state.slug, falcorData.slug) ||
+        this.isChanged(this.state.job_title, falcorData.job_title) ||
+        this.isChanged(this.state.image, falcorData.image) ||
+        this.isChanged(this.state.biography, falcorData.biography);
+
+      if (changedFlag !== this.state.changed) {
+        this.safeSetState({changed: changedFlag});
+      }
+    }, 500);
+
     this.debouncedHandleFormChanges = debounce((event) => {
+      console.log(this.state.name);
+      console.log(this.state.slug);
+      console.log(this.state.biography);
+
       // We don't want the debounced event to happen if we're saving
       if (this.state.saving) return;
 
@@ -61,11 +89,23 @@ export default class EditorAuthorController extends FalcorController {
 
   componentWillMount() {
     const falcorCallback = (data) => {
+      let name = data.authorsBySlug[this.props.params.slug].name;
+      let slug = data.authorsBySlug[this.props.params.slug].slug;
+      let image = data.authorsBySlug[this.props.params.slug].image;
+      let job_title = data.authorsBySlug[this.props.params.slug].job_title;
       let biography = data.authorsBySlug[this.props.params.slug].biography;
-      if (!biography) {
-        biography = "";
-      }
+
+      if (!name) { name = ""; }
+      if (!slug) { slug = ""; }
+      if (!image) { image = ""; }
+      if (!job_title) { job_title = ""; }
+      if (!biography) { biography = ""; }
+
       this.safeSetState({
+        name: name,
+        slug: slug,
+        image: image,
+        job_title: job_title,
         biography: biography,
       });
     }
@@ -74,11 +114,24 @@ export default class EditorAuthorController extends FalcorController {
 
   componentWillReceiveProps(nextProps) {
     const falcorCallback = (data) => {
+
+      let name = data.authorsBySlug[this.props.params.slug].name;
+      let slug = data.authorsBySlug[this.props.params.slug].slug;
+      let image = data.authorsBySlug[this.props.params.slug].image;
+      let job_title = data.authorsBySlug[this.props.params.slug].job_title;
       let biography = data.authorsBySlug[this.props.params.slug].biography;
-      if (!biography) {
-        biography = "";
-      }
+
+      if (!name) { name = ""; }
+      if (!slug) { slug = ""; }
+      if (!image) { image = ""; }
+      if (!job_title) { job_title = ""; }
+      if (!biography) { biography = ""; }
+
       this.safeSetState({
+        name: name,
+        slug: slug,
+        image: image,
+        job_title: job_title,
         biography: biography,
       });
     }
@@ -93,6 +146,13 @@ export default class EditorAuthorController extends FalcorController {
     event.preventDefault();
 
     const formNode = event.target;
+
+    const name = this.state.name;
+    const slug = this.state.slug;
+    const job_title = this.state.job_title;
+    const image = this.state.image;
+    const biography = this.state.biography;
+
     const authorSlug = this.props.params.slug
 
     // Gets all the input elements that we named
@@ -180,16 +240,31 @@ the save changes button is supposed to be disabled in this case");
   }
 
   handleBiographyChanges(e) {
-    let biography = e.target.value;
-    if (markdownLength(biography) > MAX_BIOGRAPHY_LENGTH) {
-      biography = biography.substr(0, MAX_BIOGRAPHY_LENGTH);
+    this.safeSetState({ biography: e.target.value });
+    let bio = this.state.biography;
+    if (markdownLength(bio) > MAX_BIOGRAPHY_LENGTH) {
+      bio = bio.substr(0, MAX_BIOGRAPHY_LENGTH);
+      this.safeSetState({ biography: bio });
     }
-    this.safeSetState({
-      biography: biography,
-    });
+  }
+
+  isChanged (userInput, falcorData) {
+    return ((userInput !== falcorData) && !(!userInput && !falcorData));
   }
 
   render() {
+    const styles = {
+      authorProfile: {
+        paddingLeft: 30,
+        paddingRight: 30,
+        paddingBottom: 30,
+        paddingTop: 10,
+      },
+      buttons: {
+        marginTop: 12,
+        marginBottom: 12,
+      },
+    }
     if (this.state.ready) {
       if (!this.state.data) {
         return <div><p>No authors match the slug given in the URL</p></div>;
@@ -211,7 +286,7 @@ the save changes button is supposed to be disabled in this case");
       }
       else {
         if (!this.state.saving) {
-          changedStateMessage = "Unsaved Changes";
+          changedStateMessage = "Save Changes";
           changedStateStyle.color = "red";
         }
         else {
@@ -221,12 +296,72 @@ the save changes button is supposed to be disabled in this case");
       }
 
       return (
-        <div>
-          <h2 style={changedStateStyle}>{changedStateMessage}</h2>
-          <h3>{author.name}</h3>
+        <div style={styles.authorProfile}>
+          <h3>Author Profile: {this.state.name}</h3>
+          <Divider />
           <p>Change the information for the author and press Save Changes to confirm the changes.</p>
+          <form onSubmit={this.handleSaveChanges}>
+            <TextField
+              value={this.state.name}
+              floatingLabelText="Name"
+              disabled={this.state.saving}
+              onChange={e => {
+                this.safeSetState({ name: e.target.value });
+                this.debouncedHandleFormStateChanges();}}
+            /><br />
+            <TextField
+              value={this.state.slug}
+              floatingLabelText="Slug"
+              disabled={this.state.saving}
+              onChange={e => {
+                this.safeSetState({ slug: e.target.value });
+                this.debouncedHandleFormStateChanges();}}
+            /><br />
+            <TextField
+              value={this.state.job_title}
+              floatingLabelText="Job Title"
+              disabled={this.state.saving}
+              onChange={e => {
+                this.safeSetState({ job_title: e.target.value });
+                this.debouncedHandleFormStateChanges();}}
+            /><br />
+            <TextField
+              name="image"
+              value={this.state.image}
+              floatingLabelText="Image (Remember to use https:// not http://)"
+              disabled={this.state.saving}
+              onChange={e => {
+                this.safeSetState({ image: e.target.value });
+                this.debouncedHandleFormStateChanges();}}
+              fullWidth
+            /><br />
+            <TextField
+              name="biography"
+              floatingLabelText={"Biography (" + markdownLength(this.state.biography) +
+                " of " + MAX_BIOGRAPHY_LENGTH + " characters)"}
+              value={this.state.biography}
+              disabled={this.state.saving}
+              onChange={this.handleBiographyChanges}
+              multiLine
+              rows={2}
+              rowsMax={5}
+              fullWidth
+            /><br />
+            <RaisedButton
+              label={changedStateMessage}
+              primary
+              style={styles.buttons}
+              disabled={!this.state.changed || this.state.saving}
+            />
+          </form>
+
+
+
+
+          <br />
+          <br />
+          <br />
           <form
-            className="pure-form pure-form-stacked"
             onChange={(e) => {e.persist(); this.debouncedHandleFormChanges(e)}}
             onSubmit={this.handleSaveChanges}
           >
@@ -269,6 +404,7 @@ the save changes button is supposed to be disabled in this case");
               name="biography"
               disabled={this.state.saving}
             />
+            <p style={changedStateStyle}>{changedStateMessage}</p>
             <input
               className="pure-button pure-button-primary"
               type="submit"
@@ -281,8 +417,7 @@ the save changes button is supposed to be disabled in this case");
       );
     }
     else {
-      return <CircularProgress />
-;
+      return (<CircularProgress />);
     }
   }
 }
