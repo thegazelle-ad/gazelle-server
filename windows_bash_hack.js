ca7b141bb8b450cc8f98ed39c96b2471a3772672
Shantanu Bhatia <sb5455@nyu.edu>
2017-06-16 18:02:19 +0400

    Added absolute URL check
    Alerts if articles contain <a> tags where the href value does not start
    with http

--- windows_bash_hack.js
+++ windows_bash_hack.js
@@ -1,4 +1,5 @@
 // Hack for Ubuntu on Windows: interface enumeration fails with EINVAL, so return empty.
+// All hail the Stack Overflow gods.
 try {
   require('os').networkInterfaces();
 } catch (e) {
