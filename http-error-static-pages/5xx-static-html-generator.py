3fd31b43d4c32170ce0b9a52dfdf38eded7020fd
jaisal1024 <jaisal1024@gmail.com>
2017-07-14 05:47:37 +0000

    [#122] Add 5xx error page (#172)
    * add: error 500 page static html v1
    
    * update: removed line break, added favicon, and fixed scaling and error 500 text issues
    
    * update: favicons and header changes based on github PR comments
    
    * Made error pages prouction ready

---
+++ http-error-static-pages/5xx-static-html-generator.py
@@ -0,0 +1,13 @@
+template = open('./5xx.template.html', 'r')
+templateString = template.read()
+template.close()
+
+# We only use 0-11 according to
+# https://en.wikipedia.org/wiki/List_of_HTTP_status_codes#5xx_Server_error
+for i in range(12):
+    numString = str(i) if i >= 10 else '0{}'.format(i)
+    fileName = './build/5{}.html'.format(numString)
+    outputFile = open(fileName, 'w')
+    htmlString = templateString.replace('{ERROR_CODE_PLACEHOLDER}', '5{}'.format(numString))
+    outputFile.write(htmlString)
+    outputFile.close()
