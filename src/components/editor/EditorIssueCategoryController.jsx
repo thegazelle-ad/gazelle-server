c15da9b77720050e7b7fcd4ac6a2ab4ab06cbe85
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-08-13 02:22:33 +0200

    Minor refactor of EditorIssueCategoryController

--- src/components/editor/EditorIssueCategoryController.jsx
+++ src/components/editor/EditorIssueCategoryController.jsx
@@ -8,6 +8,29 @@ import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 import CircularProgress from 'material-ui/CircularProgress';
 
+const styles = {
+  paper: {
+    height: '100%',
+    width: '100%',
+    marginTop: 20,
+    marginBottom: 20,
+    textAlign: 'left',
+    display: 'inline-block',
+  },
+  innerPaper: {
+    margin: 20,
+  },
+  tabs: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingBottom: 15,
+  },
+  buttons: {
+    marginTop: 12,
+    marginBottom: 24,
+  },
+}
+
 export default class EditorIssueCategoryController extends FalcorController {
   constructor(props) {
     super(props);
@@ -32,9 +55,7 @@ export default class EditorIssueCategoryController extends FalcorController {
     const falcorCallBack = (data) => {
       const issueNumber = this.props.params.issueNumber;
       const categories = data.issuesByNumber[issueNumber].categories;
-      const categoriesArray = _.map(categories, (category) => {
-        return category;
-      });
+      const categoriesArray = _.map(categories, category => category);
 
       this.safeSetState({
         categories: categoriesArray,
@@ -47,9 +68,7 @@ export default class EditorIssueCategoryController extends FalcorController {
     const falcorCallBack = (data) => {
       const issueNumber = this.props.params.issueNumber;
       const categories = data.issuesByNumber[issueNumber].categories;
-      const categoriesArray = _.map(categories, (category) => {
-        return category;
-      });
+      const categoriesArray = _.map(categories, category => category);
       this.safeSetState({
         categories: categoriesArray,
       });
@@ -97,7 +116,7 @@ export default class EditorIssueCategoryController extends FalcorController {
 
     const callback = (data) => {
       const updatedCategories = _.map(data.issuesByNumber[issueNumber].categories,
-        (category) => {return category});
+        category => category);
       this.safeSetState({
         changed: false,
         categories: updatedCategories,
@@ -113,29 +132,6 @@ export default class EditorIssueCategoryController extends FalcorController {
   }
 
   render() {
-    const styles = {
-      paper: {
-        height: '100%',
-        width: '100%',
-        marginTop: 20,
-        marginBottom: 20,
-        textAlign: 'left',
-        display: 'inline-block',
-      },
-      innerPaper: {
-        margin: 20,
-      },
-      tabs: {
-        paddingLeft: 30,
-        paddingRight: 30,
-        paddingBottom: 15,
-      },
-      buttons: {
-        marginTop: 12,
-        marginBottom: 24,
-      },
-    }
-
     if (this.state.ready) {
       if (!this.state.data) {
         return (
