d9f8c2d50a59c15fd7692b4c2f1323e8f905f564
Arantza Rodriguez <air258@nyu.edu>
2017-08-16 06:59:52 -0500

    Add Instant Load Transition to TextPage (#181)
    * added new component with css for transition
    
    * edited TextPage to include the transition
    
    * added transitions/text-page-load import to main.scss
    
    * returned TextPage to original state
    
    * edited TextPageController to include transition
    
    * added color variable
    
    * new css component with instant load transition
    
    * Added padding to top and fixed page responsiveness
    
    * Fixed space lint error

---
+++ src/transitions/TextPageLoad.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BaseComponent from 'lib/BaseComponent';
+
+export default class TextPageLoad extends BaseComponent {
+  render() {
+    return (
+      <div className="text-page-load">
+        <h2 className="text-page__title-load"></h2>
+        <div className="text-page__html-load"></div>
+      </div>
+    );
+  }
+}
