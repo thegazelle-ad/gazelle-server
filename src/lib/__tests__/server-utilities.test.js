eeb8733793dc9a9f1f04acd654aad9138b58551a
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-08-17 20:24:47 +0200

    Added tests for the server.js refactor

---
+++ src/lib/__tests__/server-utilities.test.js
@@ -0,0 +1,32 @@
+import { md5Hash } from 'lib/server-utilities';
+import crypto from 'crypto';
+jest.unmock('lib/server-utilities');
+jest.unmock('crypto');
+
+describe('md5Hash', () => {
+  const indexHash = md5Hash('./src/index.js');
+  it('returns a string', () => {
+    expect(typeof indexHash).toBe('string');
+  });
+
+  it('hashes consistently', () => {
+    const secondIndexHash = md5Hash('./src/index.js');
+    expect(indexHash).toBe(secondIndexHash);
+  });
+
+  it('throws on invalid path', () => {
+    expect(md5Hash.bind(null, './gibberish')).toThrow();
+  });
+
+  it('hashes different files differently', () => {
+    const adminClientHash = md5Hash('./src/client-scripts/admin-client.js');
+    expect(indexHash).not.toBe(adminClientHash);
+  });
+
+  it("doesn't just hash file names", () => {
+    const fileName = './src/index.js';
+    const hashInstance = crypto.createHash('md5');
+    const fileNameHash = hashInstance.update(fileName).digest('hex');
+    expect(fileNameHash).not.toBe(md5Hash(fileName));
+  });
+});
