cf7281330a99d441681182f0a057fdb6a9f0777f
Zane Mountcastle <zane.mountcastle@gmail.com>
2017-08-19 17:35:07 +0200

    Added image compression
    re-disable linting on tests temporarily to leave it for another PR
    
    Deleted outdated sample-issue data
    
    Changed non React component files to lowercase with dash file names
    
    Removed whitespace in server-side render markup for React error
    
    Fix upload path and delete tmp image upload files in dev
    
    Hotfix to slightly better Admin security (#191)
    
    * Changed hash algorithm to SHA512
    
    * Changed hardcoded hash
    
    * Restyled sign in page
    
    Fix restart server hash post hash function fix (#192)
    
    Added imagemin import
    
    Moved compression from client to admin server
    
    Added compression to dev mode
    
    Moved image compression to server-utilities
    
    Added tests for compression functions
    
    Finished compressJPEG tests
    
    Moved Jest unmocks to test file
    
    Update CircleCI to v2 (#194)
    
    Added imagemin import
    
    Moved compression from client to admin server
    
    Added compression to dev mode
    
    Moved image compression to server-utilities
    
    Added tests for compression functions
    
    Finished compressJPEG tests
    
    Moved Jest unmocks to test file
    
    Added image compression
    
    re-disable linting on tests temporarily to leave it for another PR
    
    Deleted outdated sample-issue data
    
    Changed non React component files to lowercase with dash file names
    
    Removed whitespace in server-side render markup for React error
    
    Fix upload path and delete tmp image upload files in dev
    
    Hotfix to slightly better Admin security (#191)
    
    * Changed hash algorithm to SHA512
    
    * Changed hardcoded hash
    
    * Restyled sign in page
    
    Fix restart server hash post hash function fix (#192)
    
    Added imagemin import
    
    Moved compression from client to admin server
    
    Added compression to dev mode
    
    Moved image compression to server-utilities
    
    Added tests for compression functions
    
    Finished compressJPEG tests
    
    Moved Jest unmocks to test file
    
    Update CircleCI to v2 (#194)
    
    Added imagemin import
    
    Moved compression from client to admin server
    
    Added compression to dev mode
    
    Moved image compression to server-utilities
    
    Added tests for compression functions
    
    Finished compressJPEG tests
    
    Moved Jest unmocks to test file
    
    Updated shrinkwrap
    
    Fixed ./tmp/ missing test error
    
    Addressed suggested changes
    
    Fixed linting errors
    
    Fixed shrinkwrap
    
    Updates server utility tests for Jest v20
    
    Made adjustments to compressJPEG tests
    
    Moved test image to tests directory
    
    Promisified deleteFile
    
    Simplified deleteFile util function
    
    Simplified copyFile rejections
    
    Addressed review comments
    
    Resolved issues in deleteFile and copyFile tests
    
    Removed unused import

--- src/lib/server-utilities.js
+++ src/lib/server-utilities.js
@@ -1,9 +1,63 @@
 import fs from 'fs';
+import Promise from 'bluebird';
+
+// md5Hash packages
 import crypto from 'crypto';
+
+// compressJPEG packages
+import imagemin from 'imagemin';
+import imageminJpegRecompress from 'imagemin-jpeg-recompress';
 
 export function md5Hash(file) {
   const hashInstance = crypto.createHash('md5');
   // readFileSync in the syncronous version of readFile
   const fileContents = fs.readFileSync(file, 'utf8');
   return hashInstance.update(fileContents).digest('hex');
+}
+
+/*
+ *  Compresses JPEG image passed as file path and overwrites uncompressed file
+ *  Returns imagemin Promise
+ */
+export function compressJPEG(filePath) {
+  const fileDirectory = filePath.substring(0, filePath.lastIndexOf('/'));
+  return imagemin([filePath], fileDirectory, {
+    plugins: [
+      imageminJpegRecompress(true), // Favor accuracy over speed
+    ],
+  });
+}
+
+/* Deletes file passed as file path */
+export const deleteFile = Promise.promisify(fs.unlink);
+
+/*
+ * Returns a promise for file copying by piping a read/write stream
+ * Thanks to the fine folks on StackOverflow:
+ * https://stackoverflow.com/questions/11293857/fastest-way-to-copy-file-in-node-js
+ */
+export function copyFile(source, target) {
+  return new Promise((resolve, reject) => {
+    fs.exists(source, (doesExist) => {
+      if (doesExist) {
+        const rd = fs.createReadStream(source);
+        rd.on('error', (error) => {
+          rd.destroy();
+          reject(error);
+        });
+
+        const wr = fs.createWriteStream(target);
+        wr.on('error', (error) => {
+          wr.end();
+          rd.destroy();
+          reject(error);
+        });
+
+        wr.on('finish', resolve);
+        rd.pipe(wr);
+      } else {
+        reject(new Error('Source file does not exist.'));
+      }
+    });
+  });
 }
