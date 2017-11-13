ef7ffa55fefa2573811accb57fdc41615bac365e
Adam Nagy <adam@linux-1t8l.suse>
2017-11-13 15:26:11 -0500

    added readme page

---
+++ src/components/editor/EditorReadme.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BaseComponent from 'lib/BaseComponent';
+
+export default class Readme extends BaseComponent {
+  render() {
+    return (
+      <div>
+        <h2>Hell Yeah boy</h2>
+        <p>The page you are looking for does exist.</p>
+      </div>
+    );
+  }
+}
