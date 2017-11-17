2b445ed47146bdcc546cb3f62f26dab3f6d2f530
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-11-17 15:25:54 -0500

    Add server side rendering tests for all thegazelle.org page types

--- __tests__/end-to-end/thegazelle.test.js
+++ __tests__/end-to-end/thegazelle.test.js
@@ -1,5 +1,7 @@
 import Nightmare from 'nightmare';
 
+const HOST = 'http://localhost:3000';
+
 jest.setTimeout(1000 * 10); // 10 second time out limit for async tests
 
 describe('The Gazelle server side rendering', () => {
@@ -8,12 +10,30 @@ describe('The Gazelle server side rendering', () => {
     nightmare = new Nightmare();
   });
 
-  it('renders front page correctly', () => {
-    expect.assertions(1);
-    return nightmare.goto('http://localhost:3000')
-      .end()
-      .then(result => {
-        expect(result.code).toBe(200);
-      });
-  });
+  const testPathServersideRender = (path, name, code = 200) => {
+    it(`renders ${name} page correctly`, () => {
+      expect.assertions(1);
+      return nightmare.goto(`${HOST}${path}`)
+        .end()
+        .then(result => {
+          expect(result.code).toBe(code);
+        });
+    });
+  };
+
+  testPathServersideRender('', 'front');
+  testPathServersideRender('/team', 'team');
+  testPathServersideRender('/archives', 'archive');
+  testPathServersideRender('/about', 'about');
+  testPathServersideRender('/ethics', 'code of ethics');
+  testPathServersideRender('/category/news', 'category');
+  testPathServersideRender('/issue/100', 'non-default issue');
+  testPathServersideRender('/search?q=abu%20dhabi', 'search');
+  testPathServersideRender(
+    '/issue/100/letters/letter-from-the-editors-celebrating-100-issues',
+    'article'
+  );
+  testPathServersideRender('/author/khadeeja-farooqui', 'author');
+  // This should definitely return a 404 but it isn't implemented right now so we'll make it a todo
+  testPathServersideRender('/this/path/should/not/exist', 'not found', 200);
 });
