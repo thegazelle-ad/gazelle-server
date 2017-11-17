9e86eb75903fb39604fd390f2f1d06e825df3c24
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-11-17 16:53:47 -0500

    Add server side rendering tests for the admin interface

---
+++ __tests__/end-to-end/admin-tests/serverside-rendering.test.js
@@ -0,0 +1,42 @@
+/**
+ * Since we don't actually server side render on the admin page as it's unnecessary
+ * we don't need to test all the pages like we do for the main website as all the server
+ * does is just send the client side script anyway.
+ */
+import Nightmare from 'nightmare';
+
+import { testPathServersideRender, SIMPLE_TEST_TIMEOUT, ADMIN_HOST as HOST } from '../utilities';
+
+jest.setTimeout(SIMPLE_TEST_TIMEOUT);
+
+describe('Admin interface server side rendering', () => {
+  testPathServersideRender(HOST, '/login', 'any');
+
+  it('correctly redirects to the login page', () => {
+    const nightmare1 = new Nightmare();
+
+    expect.assertions(2);
+
+    const allPromises = [];
+
+    allPromises.push(
+      nightmare1.goto(`${HOST}`)
+      .end()
+      .then(result => {
+        expect(result.url).toBe(`${HOST}/login?url=/`);
+      })
+    );
+
+    const nightmare2 = new Nightmare();
+
+    allPromises.push(
+      nightmare2.goto(`${HOST}/authors`)
+      .end()
+      .then(result => {
+        expect(result.url).toBe(`${HOST}/login?url=/authors`);
+      })
+    );
+
+    return Promise.all(allPromises);
+  });
+});
--- __tests__/end-to-end/gazelle-tests/serverside-rendering.test.js
+++ __tests__/end-to-end/gazelle-tests/serverside-rendering.test.js
@@ -1,39 +1,22 @@
-import Nightmare from 'nightmare';
+import { testPathServersideRender, SIMPLE_TEST_TIMEOUT, GAZELLE_HOST as HOST } from '../utilities';
 
-const HOST = 'http://localhost:3000';
-
-jest.setTimeout(1000 * 10); // 10 second time out limit for async tests
+jest.setTimeout(SIMPLE_TEST_TIMEOUT);
 
 describe('The Gazelle server side rendering', () => {
-  let nightmare = null;
-  beforeEach(() => {
-    nightmare = new Nightmare();
-  });
-
-  const testPathServersideRender = (path, name, code = 200) => {
-    it(`renders ${name} page correctly`, () => {
-      expect.assertions(1);
-      return nightmare.goto(`${HOST}${path}`)
-        .end()
-        .then(result => {
-          expect(result.code).toBe(code);
-        });
-    });
-  };
-
-  testPathServersideRender('', 'front');
-  testPathServersideRender('/team', 'team');
-  testPathServersideRender('/archives', 'archive');
-  testPathServersideRender('/about', 'about');
-  testPathServersideRender('/ethics', 'code of ethics');
-  testPathServersideRender('/category/news', 'category');
-  testPathServersideRender('/issue/100', 'non-default issue');
-  testPathServersideRender('/search?q=abu%20dhabi', 'search');
+  testPathServersideRender(HOST, '', 'front');
+  testPathServersideRender(HOST, '/team', 'team');
+  testPathServersideRender(HOST, '/archives', 'archive');
+  testPathServersideRender(HOST, '/about', 'about');
+  testPathServersideRender(HOST, '/ethics', 'code of ethics');
+  testPathServersideRender(HOST, '/category/news', 'category');
+  testPathServersideRender(HOST, '/issue/100', 'non-default issue');
+  testPathServersideRender(HOST, '/search?q=abu%20dhabi', 'search');
   testPathServersideRender(
+    HOST,
     '/issue/100/letters/letter-from-the-editors-celebrating-100-issues',
     'article'
   );
-  testPathServersideRender('/author/khadeeja-farooqui', 'author');
+  testPathServersideRender(HOST, '/author/khadeeja-farooqui', 'author');
   // This should definitely return a 404 but it isn't implemented right now so we'll make it a todo
-  testPathServersideRender('/this/path/should/not/exist', 'not found', 200);
+  testPathServersideRender(HOST, '/this/path/should/not/exist', 'not found', 200);
 });
