35654aacadb05fd136c0e8251564ae27b64566df
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-11-23 00:17:09 -0500

    Upgraded AuthorList test to check changes propagate to database

--- __tests__/end-to-end/admin-tests/e2e-admin-utilities.js
+++ __tests__/end-to-end/admin-tests/e2e-admin-utilities.js
@@ -20,3 +20,27 @@ export function getLoggedInState(path) {
     // Press enter
     .type(passwordInputSelector, '\u000d');
 }
+
+export function restartServer() {
+  const headerSelector = '#app-header';
+  const headerMenuButtonSelector = `${headerSelector}-menu-button`;
+  const restartServerButtonSelector = `${headerSelector}-restart-button`;
+  const restartServerPasswordInputSelector = '#restart-server-password-input';
+
+  return getLoggedInState('')
+    // We inject a script that sets window.THE_GAZELLE.serverRestartedSuccessfully = true when
+    // the correct `window.alert` call has been made
+    .inject('js', `${__dirname}/assets/checkServerRestarted.js`)
+    .wait(headerMenuButtonSelector)
+    // mouseup for Material UI quirk
+    .mouseup(headerMenuButtonSelector)
+    .wait(restartServerButtonSelector)
+    .click(restartServerButtonSelector)
+    .wait(restartServerPasswordInputSelector)
+    // Insert restart server password
+    .insert(restartServerPasswordInputSelector, process.env.CIRCLECI_ADMIN_PASSWORD)
+    // Press enter
+    .type(restartServerPasswordInputSelector, '\u000d')
+    .wait(() => window.THE_GAZELLE.serverRestartedSuccessfully)
+    .end();
+}
