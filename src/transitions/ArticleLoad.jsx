564dbccc0d024c41cf79c8627ce45d7ac8c1a57c
Arantza Rodriguez <arantzardzm@hotmail.com>
2017-11-26 16:53:05 +0400

    article instant load transition -- still need to fix responsiveness

---
+++ src/transitions/ArticleLoad.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BaseComponent from 'lib/BaseComponent';
+
+export default class ArticleLoad extends BaseComponent {
+  render() {
+    return (
+      <div className="article-load">
+        <h2 className="article__header-load"></h2>
+        <div className='article__image-load'></div>
+        <div className="article__body-load"></div>
+      </div>
+    );
+  }
+}
