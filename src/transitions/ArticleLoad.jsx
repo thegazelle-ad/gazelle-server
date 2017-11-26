39af554ae90c270d849adc5e0ba9e44129f31411
Arantza Rodriguez <arantzardzm@hotmail.com>
2017-11-26 16:57:05 +0400

    fixed lint error

--- src/transitions/ArticleLoad.jsx
+++ src/transitions/ArticleLoad.jsx
@@ -6,7 +6,7 @@ export default class ArticleLoad extends BaseComponent {
     return (
       <div className="article-load">
         <h2 className="article__header-load"></h2>
-        <div className='article__image-load'></div>
+        <div className="article__image-load"></div>
         <div className="article__body-load"></div>
       </div>
     );
