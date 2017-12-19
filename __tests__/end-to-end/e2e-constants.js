b3cf02fe8359f37379da0e008832c3a2221455d4
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-12-19 09:54:31 -0500

    Increase nightmare action timeout on CI for e2e tests

--- __tests__/end-to-end/e2e-constants.js
+++ __tests__/end-to-end/e2e-constants.js
@@ -1,6 +1,6 @@
 export const ENTER_UNICODE = '\u000d';
 export const SIMPLE_TEST_TIMEOUT = 10 * 1000;
-const NIGHTMARE_ACTION_TIMEOUT = (process.env.CIRCLECI ? 4 : 2) * 1000;
+const NIGHTMARE_ACTION_TIMEOUT = (process.env.CIRCLECI ? 6 : 2) * 1000;
 export const NIGHTMARE_CONFIG = {
   waitTimeout: NIGHTMARE_ACTION_TIMEOUT,
   gotoTimeout: NIGHTMARE_ACTION_TIMEOUT,
