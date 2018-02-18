b93b3394e9597f84eeadbb4e6ef8c96cfdb870e6
Xin (Cynthia) Tong <xin.tong@nyu.edu>
2018-02-18 18:30:16 +0400

    Add E2E test for admin not found page (#282)

---
+++ __tests__/end-to-end/admin-tests/notfound.test.js
@@ -0,0 +1,26 @@
+import Nightmare from 'nightmare';
+
+import {
+  NIGHTMARE_CONFIG,
+} from '__tests__/end-to-end/e2e-constants';
+import { getLoggedInState } from './e2e-admin-utilities';
+
+describe('Not Found Page', () => {
+  let nightmare = null;
+
+  afterEach(() => {
+    // kill the nightmare instance
+    nightmare.halt();
+  });
+
+  it('renders not-found page for wildcard route',
+    () => {
+      nightmare = new Nightmare(NIGHTMARE_CONFIG);
+      const notFoundPageSelector = '#not-found-page';
+
+      return getLoggedInState(nightmare, '/not/a/valid/route')
+      .wait(notFoundPageSelector)
+      .end();
+    }
+  );
+});
