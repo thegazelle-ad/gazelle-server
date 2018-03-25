9e4d732a618fc370f2c42aa23c974651fe897ffb
helw150 <wbh230@nyu.edu>
2018-03-25 14:08:56 +0400

    Need to source NVM

--- scripts/helpers/develop.sh
+++ scripts/helpers/develop.sh
@@ -1,1 +1,2 @@
+source ~/.nvm/nvm.sh
 nvm use 4 && cd ~/database-and-ghost-blog-server/ && npm start --production & sleep 2 && nvm use 9 && cd ~/gazelle-server/ && npm run build:watch & sleep 5 && npm start && fg && fg
