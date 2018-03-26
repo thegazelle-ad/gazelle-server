ef4bf74512f63af403ecbd2bc1bd9615d46b1208
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-03-26 11:18:50 +0400

    Cleanup tag tables

---
+++ migrations/20180326110614_cleanup-tag-tables.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+exports.up = async knex => {
+  await Promise.all([
+    knex.schema.alterTable('articles_tags', table => {
+      table.dropColumn('sort_order');
+    }),
+    knex.schema.alterTable('tags', table => {
+      const tablesToDrop = [
+        'uuid',
+        'description',
+        'image',
+        'parent_id',
+        'visibility',
+        'meta_title',
+        'meta_description',
+        'created_at',
+        'created_by',
+        'updated_at',
+        'updated_by',
+      ]
+      tablesToDrop.forEach(name => table.dropColumn(name));
+    })
+  ]);
+};
+
+exports.down = knex => {
+  // It is too bothersome to write the reverse migration here, so I simply won't <.<
+};
