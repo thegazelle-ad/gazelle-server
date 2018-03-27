6831f388aaeefb1d29a288d471094543afb82c87
helw150 <wbh230@nyu.edu>
2018-03-27 13:00:46 +0400

    Add comment on reason for max length     The reason for a file like this should be that it makes it very clear why these constant exist     I should set a good example by explaining the MAX_TEASER_LENGTH so future MAX_ follow suit

--- src/components/admin/lib/max-field-lengths.js
+++ src/components/admin/lib/max-field-lengths.js
@@ -2,4 +2,8 @@
  * File to store and export the maximum values for any text fields
  */
 
+/*
+ * Teaser length is constrained to 156 characters so
+ * that the teaser is not cut off by search engines
+ */
 export const MAX_TEASER_LENGTH = 156;
