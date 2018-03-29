b4273d57d09f1cc32bff7a2eac402cfdb289cff9
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-03-30 00:39:22 +0400

    install prettier and upgrade eslint

---
+++ .eslintrc.js
@@ -0,0 +1,82 @@
+module.exports = {
+  root: true,
+  plugins: ['react', 'import', 'jsx-a11y'],
+  settings: {
+    resolve: {
+      root: 'src',
+    },
+    react: {
+      pragma: 'React',
+      version: '15.0',
+    },
+    'import/resolver': {
+      webpack: {
+        config: 'webpack/webpack.dev.config.js',
+        'config-index': 0,
+      },
+    },
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
+    },
+    ecmaVersion: 9,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    // We want to re-enable no-alert when we have something to replace it with
+    'no-alert': 'off',
+    'no-use-before-define': 'off',
+    'valid-jsdoc': [
+      'error',
+      {
+        prefer: {
+          arg: 'param',
+          argument: 'param',
+          class: 'constructor',
+          return: 'returns',
+          virtual: 'abstract',
+        },
+        preferType: {
+          Boolean: 'boolean',
+          Number: 'number',
+          object: 'Object',
+          String: 'string',
+        },
+        requireReturn: false,
+        requireReturnType: true,
+        requireReturnDescription: false,
+        requireParamDescription: false,
+      },
+    ],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'react/no-multi-comp': 'off',
+    'class-methods-use-this': 'off',
+    'func-names': ['error', 'as-needed'],
+    'jsx-a11y/no-autofocus': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      { components: ['Link'], specialLink: ['to'] },
+    ],
+    // Prettier handles this for us
+    'react/jsx-indent': 'off',
+    // Emil: We might contemplate adding in these rules later, some of them I'm disabling because
+    // I find them overly restrictive, others simply because I don't want to fix them right now
+    'import/first': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: 'babylon',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
