46877fc7a70dbf94dc3c977bb7fa5d5ed4353352
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-04-09 18:15:21 +0400

    Complete comment for down migration on cleanup-tag-tables

--- migrations/20180326110614_cleanup-tag-tables.js
+++ migrations/20180326110614_cleanup-tag-tables.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 exports.up = async knex => {
   await Promise.all([
     knex.schema.alterTable('articles_tags', table => {
@@ -17,12 +16,16 @@ exports.up = async knex => {
         'created_by',
         'updated_at',
         'updated_by',
-      ]
+      ];
       columnsToDrop.forEach(name => table.dropColumn(name));
-    })
+    }),
   ]);
 };
 
+// eslint-disable-next-line no-unused-vars
 exports.down = knex => {
-  // It is too bothersome to write the reverse migration here, so I simply won't <.<
+  // It is probably impossible to revert all the lost data that is
+  // delete through the migration as it was no longer needed so
+  // we don't even try this as there are not nullable columns
+  // without default values that we dropped, so we can't make it work
 };
