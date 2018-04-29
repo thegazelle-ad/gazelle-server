b9f5db63680a03a7743f25657d652d731c831b91
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-04-29 18:36:41 +0400

    Fix e2e test file regex

--- __tests__/config/jest.e2e.config.js
+++ __tests__/config/jest.e2e.config.js
@@ -2,6 +2,6 @@ const configBase = require('./jest.base.config.js');
 
 module.exports = {
   ...configBase,
-  testRegex: String.raw`.*/__tests__/end-to-end/.*\.test\.js`,
+  testRegex: String.raw`.*/__tests__/end-to-end/.*\.test\.js$`,
   setupTestFrameworkScriptFile: '<rootDir>/__tests__/end-to-end/jest-setup.js',
 };
