91a6b271132aab919525fb534babc16be656b2bd
Nav <navyasuri@nyu.edu>
2018-07-06 19:43:44 +0530

    Added changes to make it synchronous

--- src/lib/falcor/routes/search/__tests__/search-finds.test.js
+++ src/lib/falcor/routes/search/__tests__/search-finds.test.js
@@ -6,7 +6,7 @@ import {
   cleanupTestDatabase,
 } from '__tests__/helpers/db-helpers';
 import path from 'path';
-import { titles, slugs, markdowns } from './dummy-search-articles';
+import { dummyArticles } from './dummy-search-articles';
 
 const name = path
   .basename(__filename)
@@ -18,28 +18,27 @@ const databaseName = `gazelle_test_${name}_${new Date().getTime()}`;
 const database = getDatabaseConnection(databaseName);
 
 afterAll(() => database.destroy());
-describe('findRelaventArticles', () => {
-  beforeAll(() => {
-    initializeTestDatabase(database, databaseName);
-    for (let i = 0; i < 5; i++) {
-      database('articles').insert(() => ({
-        title: titles[i],
-        slug: slugs[i],
-        markdown: markdowns[i],
-      }));
-    }
+
+describe('searchScoredQuery', () => {
+  beforeAll(async () => {
+    await initializeTestDatabase(database, databaseName);
+    await Promise.all(
+      dummyArticles.map(article => database('articles').insert(article)),
+    );
   });
   afterAll(() => cleanupTestDatabase(databaseName));
-});
 
-it('searches for potato', async () => {
-  await expect(searchScoredQuery('potato', 1, 5)).toMatchSnapshot();
-});
+  it('searches for potato', async () => {
+    await expect(searchScoredQuery('potato', 1, 5)).resolves.toMatchSnapshot();
+  });
 
-it('searches for football', async () => {
-  await expect(searchScoredQuery('football', 1, 5)).toMatchSnapshot();
-});
+  it('searches for football', async () => {
+    await expect(
+      searchScoredQuery('football', 1, 5),
+    ).resolves.toMatchSnapshot();
+  });
 
-it('searches for hobbit', async () => {
-  await expect(searchScoredQuery('hobbit', 1, 5)).toMatchSnapshot();
+  it('searches for hobbit', async () => {
+    await expect(searchScoredQuery('hobbit', 1, 5)).resolves.toMatchSnapshot();
+  });
 });
