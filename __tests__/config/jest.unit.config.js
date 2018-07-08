d0d9b808f3c96bc9ea16c0f0f829764c8c05b99e
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-07-08 11:23:33 +0100

    Update jest config

--- __tests__/config/jest.unit.config.js
+++ __tests__/config/jest.unit.config.js
@@ -2,8 +2,11 @@ const configBase = require('./jest.base.config.js');
 
 module.exports = {
   ...configBase,
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/__tests__/end-to-end/',
-  ],
+  collectCoverageFrom: configBase.collectCoverageFrom.concat([
+    '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/**/*.sql.{js,ts}', // We don't test knex code in unit tests
+  ]),
+  // Only look in source folder and ignore integration tests
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.test.{js,jsx,ts,tsx}'],
+  testPathIgnorePatterns: [String.raw`.*\.it\.test\.(j|t)sx?$`],
 };
