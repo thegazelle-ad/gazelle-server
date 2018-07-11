d404f9b0d0f58021503e3b4ec585647576cb0a30
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-07-11 22:31:51 +0100

    Refactor send-to-slack script

--- deployment-resources/scripts/send-to-slack.js
+++ deployment-resources/scripts/send-to-slack.js
@@ -1,30 +1,34 @@
+/* eslint-disable strict */
+/* eslint-disable no-console */
+
 'use strict';
 
 const fs = require('fs');
+const { WebClient } = require('@slack/client');
 
-const WebClient = require('@slack/client').WebClient;
-
-const token = require('./config.js').apiToken;
-
-let message, channel;
+const token = process.env.SLACK_API_TOKEN;
+let message;
+let channel;
 if (process.argv.length === 3) {
   // Only a message was passed so we use default channel
-  channel = "deployment"
-  message = process.argv[2];
+  channel = 'deployment';
+  [, , message] = process.argv;
 } else if (process.argv.length === 4) {
-  channel = process.argv[2];
-  message = process.argv[3];
+  [, , channel, message] = process.argv;
 } else {
-  console.error("Usage: node index.js 'message to send'\nOR\nnode index.js <channel_to_send_to> 'message to send'");
+  console.error(
+    "Usage: node index.js 'message to send'\nOR\nnode index.js <channel_to_send_to> 'message to send'",
+  );
   process.exit(1);
 }
 
 const web = new WebClient(token);
-web.chat.postMessage(channel, message, {as_user: true}, function (err, res) {
+web.chat.postMessage(channel, message, { as_user: true }, err => {
   if (err) {
     const fd = fs.openSync('slack-bot-errors.log', 'a');
-    fs.writeSync(fd, new Date().toString() + ": " + err.toString() + "\n");
+    fs.writeSync(fd, `${new Date().toString()}: ${err.toString()}\n`);
     fs.closeSync(fd);
+    console.error(err);
     process.exit(1);
   }
 });
