a9fdac23e39c7f9a9fd7ba4c94f0abde847092cd
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-09-15 20:50:37 +0400

    Increase top margin in preview

--- src/components/admin/article/components/ArticlePreview.jsx
+++ src/components/admin/article/components/ArticlePreview.jsx
@@ -2,7 +2,7 @@ import ArticleBody from 'components/main/ArticleBody';
 import React from 'react';
 
 const ArticlePreview = props => (
-  <div className="article">
+  <div className="article" style={{ marginTop: '20px' }}>
     <ArticleBody {...props} />
   </div>
 );
