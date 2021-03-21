60d49edfee11f86552e3165a416f11c3d1057e78
dweggy <colacolaojm@hotmail.com>
2021-03-21 20:45:47 +0400

    added view of netids

--- src/components/admin/AccessControl.jsx
+++ src/components/admin/AccessControl.jsx
@@ -4,10 +4,12 @@ import { Link } from 'react-router';
 
 // material-ui
 import CircularProgress from 'material-ui/CircularProgress';
+import ListItem from 'material-ui/List/ListItem';
 import Paper from 'material-ui/Paper';
 import Divider from 'material-ui/Divider';
 import RaisedButton from 'material-ui/RaisedButton';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
+import List from 'material-ui/List/List';
+import Subheader from 'material-ui/Subheader';
 
 import ContentAdd from 'material-ui/svg-icons/content/add';
 
@@ -41,8 +43,8 @@ const styles = {
 class AccessController extends FalcorController {
   constructor(props) {
     super(props);
-    this.setState({
-      listOfNetIDS: ['abc72', 'abd94']
+    this.safeSetState({
+      listOfNetIDs: ['abc72', 'abd94', 'bde32', 'cd232', 'de23', 'di23823', 'ef237']
     })
   }
 
@@ -51,7 +53,8 @@ class AccessController extends FalcorController {
   }
 
   render() {
-    if (this.state.ready) {
+    const netIDList = this.state.listOfNetIDs 
+    if (this.state.listOfNetIDs) {
       return (
         <div>
           <h1>Access</h1>
@@ -60,29 +63,43 @@ class AccessController extends FalcorController {
             <div style={styles.tabs}>
               <h2> List of NetIDS with access </h2>
 
+              <Paper style={styles.paper} zDepth={1} id='cat'>
+                <List style={{ overflow: 'auto', maxHeight: '250px' }}>
+                  <Subheader>NetIDs</Subheader>
+                  {netIDList.map(netid =>
+                    (<ListItem
+                      primaryText={netid}
+                    />)
+                  )}
+                </List>
+              </Paper>
+
+              <div style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <RaisedButton
+                  label="Apply Changes"
+                  type="submit"
+                  primary
+                  style={styles.buttons}
+                  disabled={false}
+                />
+                <Link to='/'>
+                  <RaisedButton
+                    primary
+                    style={styles.buttons}
+                  >
+                    <ContentAdd color="white" />
+                  </RaisedButton>
+                </Link>
+              </div>
             </div>
             <Divider />
           </Paper>
           {this.props.children}
-          <RaisedButton
-            label="Apply Changes"
-            type="submit"
-            primary
-            style={styles.buttons}
-            disabled={false}
-          />
-          <Link to='/'>
-            <FloatingActionButton
-              style={{
-                position: 'fixed',
-                zIndex: 50,
-                bottom: '50px',
-                right: '100px',
-              }}
-            >
-            <ContentAdd />
-            </FloatingActionButton>
-          </Link>
           <br />
         </div>
       );
